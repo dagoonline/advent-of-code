bb96c5832ee555c5c5b6159835d6c828c149f674
David González <dg@databeacon.aero>
2024-12-09 22:35:29 +0100

    tmp

--- 2024/src/day9.rs
+++ 2024/src/day9.rs
@@ -1,6 +1,9 @@
+use std::collections::VecDeque;
+
 use aoc_runner_derive::{aoc, aoc_generator};
-#[aoc_generator(day9)]
-fn parse(input: &str) -> Vec<i64> {
+
+#[aoc_generator(day9, part1)]
+fn parse_part1(input: &str) -> Vec<i64> {
     let mut chars = input.chars();
     let mut blocks = vec![];
     let mut id = 0;
@@ -25,7 +28,7 @@ fn part1(input: &Vec<i64>) -> i64 {
     let mut free_space_pointer = 0;
     let mut block_space_pointer = compacted.len() - 1;
 
-    while block_space_pointer > free_space_pointer {
+    loop {
         while block_space_pointer > 0 && compacted[block_space_pointer] < 0 {
             block_space_pointer -= 1;
         }
@@ -33,9 +36,13 @@ fn part1(input: &Vec<i64>) -> i64 {
             free_space_pointer += 1;
         }
 
+        if block_space_pointer <= free_space_pointer {
+            break;
+        }
         compacted[free_space_pointer] = compacted[block_space_pointer];
         compacted[block_space_pointer] = -1;
     }
+
     checksum(&compacted[..free_space_pointer])
 }
 
@@ -49,9 +56,38 @@ fn checksum(v: &[i64]) -> i64 {
     checksum
 }
 
-// #[aoc(day9, part2)]
-fn part2(_input: &str) -> String {
-    todo!()
+#[aoc_generator(day9, part2)]
+fn parse_part2(input: &str) -> (VecDeque<(u8, u8)>, VecDeque<(u8, u8)>) {
+    let mut chars = input.chars();
+    let mut file_blocks: VecDeque<(u8, u8)> = VecDeque::new();
+    let mut free_blocks: VecDeque<(u8, u8)> = VecDeque::new();
+
+    let mut id = 0;
+    while let Some(file_block) = chars.next() {
+        file_blocks.push_back((id, file_block.to_digit(10).unwrap() as u8));
+
+        if let Some(free_space) = chars.next() {
+            free_blocks.push_back((id, free_space.to_digit(10).unwrap() as u8));
+        }
+
+        id += 1;
+    }
+
+    (file_blocks, free_blocks)
+}
+
+#[aoc(day9, part2)]
+fn part2((file_blocks, free_blocks): &(VecDeque<(u8, u8)>, VecDeque<(u8, u8)>)) -> i64 {
+    let mut consolidated = vec![];
+    let mut current_file_block = 
+
+    for (id, free_space) in free_blocks {
+        for _ in 0..*free_space {
+
+        }
+    }
+
+    part1(&blocks)
 }
 
 #[cfg(test)]
@@ -62,11 +98,11 @@ mod tests {
 
     #[test]
     fn part1_example() {
-        assert_eq!(part1(&parse(EXAMPLE)), 1928);
+        assert_eq!(part1(&parse_part1(EXAMPLE)), 1928);
     }
 
-    // #[test]
-    // fn part2_example() {
-    //     assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
-    // }
+    #[test]
+    fn part2_example() {
+        assert_eq!(part2(&parse_part2(EXAMPLE)), 2858);
+    }
 }
