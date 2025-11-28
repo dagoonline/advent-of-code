036c4fa13c2631b29496d6d2e36ff5bb5e11afe7
David González <dago@dago.co>
2025-11-28 12:33:54 +0100

    2015, Day14 part2

--- 2015/src/day14.rs
+++ 2015/src/day14.rs
@@ -1,14 +1,18 @@
+use std::collections::HashMap;
+
 use aoc_runner_derive::{aoc, aoc_generator};
 
 struct Reindeer {
+    name: String,
     speed: u32,
     duration: u32,
     rest: u32,
 }
 
 impl Reindeer {
-    fn new(speed: u32, duration: u32, rest: u32) -> Self {
+    fn new(name: String, speed: u32, duration: u32, rest: u32) -> Self {
         Reindeer {
+            name,
             speed,
             duration,
             rest,
@@ -28,6 +32,7 @@ impl Reindeer {
         self.speed * self.duration * amount + extra
     }
 }
+
 #[aoc_generator(day14)]
 fn parse(input: &str) -> Vec<Reindeer> {
     let mut reindeers = vec![];
@@ -36,6 +41,7 @@ fn parse(input: &str) -> Vec<Reindeer> {
     for line in input.lines() {
         let data = re.captures(line).unwrap();
         reindeers.push(Reindeer::new(
+            data["reindeer"].to_string(),
             data["speed"].to_string().parse().unwrap(),
             data["duration"].to_string().parse().unwrap(),
             data["rest"].to_string().parse().unwrap(),
@@ -45,14 +51,41 @@ fn parse(input: &str) -> Vec<Reindeer> {
 }
 
 #[aoc(day14, part1)]
-fn part1(input: &[Reindeer]) -> u32 {
-    input.iter().map(|r| r.distance_after(2503)).max().unwrap()
+fn part1(reindeers: &[Reindeer]) -> u32 {
+    reindeers
+        .iter()
+        .map(|r| r.distance_after(2503))
+        .max()
+        .unwrap()
 }
 
-// #[aoc(day14, part2)]
-// fn part2(input: &str) -> String {
-//     todo!()
-// }
+#[aoc(day14, part2)]
+fn part2(reindeers: &[Reindeer]) -> u32 {
+    let mut standings = HashMap::<String, u32>::new();
+
+    for time in 1..2504 {
+        let race: Vec<(String, u32)> = reindeers
+            .iter()
+            .map(|r| (r.name.clone(), r.distance_after(time)))
+            .collect();
+        let (_, max_distance) = race
+            .iter()
+            .max_by(|(_, v1), (_, v2)| v1.partial_cmp(v2).unwrap())
+            .unwrap()
+            .to_owned();
+        let ties: Vec<String> = race
+            .into_iter()
+            .filter(|(_, v)| *v == max_distance)
+            .map(|(r, _)| r)
+            .collect();
+
+        for winner in ties {
+            standings.entry(winner).and_modify(|v| *v += 1).or_insert(1);
+        }
+    }
+
+    standings.values().max().unwrap().to_owned()
+}
 
 #[cfg(test)]
 mod tests {
@@ -69,8 +102,14 @@ mod tests {
         );
     }
 
-    // #[test]
-    // fn part2_example() {
-    //     assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
-    // }
+    #[test]
+    fn part2_example() {
+        assert_eq!(
+            part2(&parse(
+                r#"Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+                Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds."#
+            )),
+            1564
+        );
+    }
 }
