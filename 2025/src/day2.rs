245dda875b181cabd73313726f34acc018f968ab
David González <dago@dago.co>
2025-12-02 11:14:05 +0100

    Beautify 2025 Day2

--- 2025/src/day2.rs
+++ 2025/src/day2.rs
@@ -1,23 +1,24 @@
-use core::str;
-
 use aoc_runner_derive::{aoc, aoc_generator};
 #[aoc_generator(day2)]
 fn parse(input: &str) -> Vec<(u64, u64)> {
     let mut ranges = vec![];
     let parts = input.split(',');
+
     for part in parts {
         let mut range = part.split('-');
         let start: u64 = range.next().unwrap().parse().unwrap();
         let end: u64 = range.next().unwrap().parse().unwrap();
+
         ranges.push((start, end));
     }
+
     ranges
 }
 
 fn is_valid_part1(n: u64) -> bool {
     let s = n.to_string();
-
     let (first, second) = s.split_at(s.len() / 2);
+
     first != second
 }
 
@@ -26,20 +27,21 @@ fn is_valid_part2(n: u64) -> bool {
     let mut chunk_size = 1;
 
     while chunk_size <= s.len() / 2 {
-        let sub = &s[0..chunk_size];
+        let first_chunk = &s[0..chunk_size];
         let mut from = chunk_size;
         let mut is_valid = false;
 
-        while !is_valid && from < s.len() {
+        while from < s.len() {
             let to = (from + chunk_size).min(s.len());
+            let chunk = &s[from..to];
 
-            if sub != &s[from..to] {
+            if chunk != first_chunk {
                 is_valid = true;
                 break;
             }
+
             from += chunk_size
         }
-
         if !is_valid {
             return false;
         }
@@ -50,23 +52,19 @@ fn is_valid_part2(n: u64) -> bool {
     true
 }
 
-fn solver(input: &Vec<(u64, u64)>, is_valid: &dyn Fn(u64) -> bool) -> u64 {
-    let mut counter = 0;
-
-    for (start, end) in input {
-        counter += (*start..*end + 1).filter(|v| !is_valid(*v)).sum::<u64>();
-    }
-
-    counter
+fn solver(input: &[(u64, u64)], is_valid: &dyn Fn(u64) -> bool) -> u64 {
+    input.iter().fold(0, |acc, (start, end)| {
+        acc + (*start..*end + 1).filter(|v| !is_valid(*v)).sum::<u64>()
+    })
 }
 
 #[aoc(day2, part1)]
-fn part1(input: &Vec<(u64, u64)>) -> u64 {
+fn part1(input: &[(u64, u64)]) -> u64 {
     solver(input, &is_valid_part1)
 }
 
 #[aoc(day2, part2)]
-fn part2(input: &Vec<(u64, u64)>) -> u64 {
+fn part2(input: &[(u64, u64)]) -> u64 {
     solver(input, &is_valid_part2)
 }
 
