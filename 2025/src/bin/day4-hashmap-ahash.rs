0fae7c4dadead9fa65bf8e7c90a46af296a2d1cc
David González <dago@dago.co>
2025-12-04 16:49:19 +0100

    2025, Day4 - Test several implementations

--- 2025/src/bin/day4-hashmap-ahash.rs
+++ 2025/src/bin/day4-hashmap-ahash.rs
@@ -1,69 +1,54 @@
-use nohash::{IsEnabled, NoHashHasher};
-use std::{collections::HashMap, hash::BuildHasherDefault};
+use ahash::{HashMap, HashMapExt};
 
-#[derive(PartialEq, Eq, Clone)]
-struct Key((usize, usize));
-
-impl std::hash::Hash for Key {
-    fn hash<H: std::hash::Hasher>(&self, hasher: &mut H) {
-        hasher.write_usize(self.0.1);
-        hasher.write_usize(self.0.1)
-    }
-}
-
-impl IsEnabled for Key {}
-
-type Data = HashMap<Key, u8, BuildHasherDefault<NoHashHasher<Key>>>;
-
-fn parse(input: &str) -> Data {
-    let mut grid: Data = HashMap::with_hasher(BuildHasherDefault::default());
+fn parse(input: &str) -> HashMap<(usize, usize), u8> {
+    let mut grid: HashMap<(usize, usize), u8> = HashMap::new();
 
     for (y, line) in input.lines().enumerate() {
         for (x, _) in line.chars().enumerate().filter(|(_, c)| *c == '@') {
-            grid.insert(Key((x + 1, y + 1)), 0);
+            grid.insert((x + 1, y + 1), 0);
         }
     }
 
-    for Key((x, y)) in grid.clone().into_keys() {
+    for (x, y) in grid.clone().into_keys() {
         let mut v = 0;
 
-        v += grid.contains_key(&Key((x - 1, y - 1))) as u8;
-        v += grid.contains_key(&Key((x, y - 1))) as u8;
-        v += grid.contains_key(&Key((x + 1, y - 1))) as u8;
-        v += grid.contains_key(&Key((x - 1, y))) as u8;
-        v += grid.contains_key(&Key((x + 1, y))) as u8;
-        v += grid.contains_key(&Key((x - 1, y + 1))) as u8;
-        v += grid.contains_key(&Key((x, y + 1))) as u8;
-        v += grid.contains_key(&Key((x + 1, y + 1))) as u8;
+        v += grid.contains_key(&(x - 1, y - 1)) as u8;
+        v += grid.contains_key(&(x, y - 1)) as u8;
+        v += grid.contains_key(&(x + 1, y - 1)) as u8;
+        v += grid.contains_key(&(x - 1, y)) as u8;
+        v += grid.contains_key(&(x + 1, y)) as u8;
+        v += grid.contains_key(&(x - 1, y + 1)) as u8;
+        v += grid.contains_key(&(x, y + 1)) as u8;
+        v += grid.contains_key(&(x + 1, y + 1)) as u8;
 
-        grid.entry(Key((x, y))).and_modify(|val| *val = v);
+        grid.entry((x, y)).and_modify(|val| *val = v);
     }
 
     grid
 }
 
-fn get_acceseable(rolls: &Data) -> u16 {
+fn get_acceseable(rolls: &HashMap<(usize, usize), u8>) -> u16 {
     rolls.iter().fold(0, |acc, (_, v)| acc + (v < &4) as u16)
 }
 
-fn step(rolls: &mut Data) {
+fn step(rolls: &mut HashMap<(usize, usize), u8>) {
     let keys = rolls
         .iter()
         .filter(|(_, v)| **v < 4)
-        .map(|(Key((x, y)), _)| (*x, *y))
+        .map(|((x, y), _)| (*x, *y))
         .collect::<Vec<(usize, usize)>>();
 
     for (x, y) in keys {
-        rolls.remove(&Key((x, y)));
+        rolls.remove(&(x, y));
 
-        rolls.entry(Key((x - 1, y - 1))).and_modify(|v| *v -= 1);
-        rolls.entry(Key((x, y - 1))).and_modify(|v| *v -= 1);
-        rolls.entry(Key((x + 1, y - 1))).and_modify(|v| *v -= 1);
-        rolls.entry(Key((x - 1, y))).and_modify(|v| *v -= 1);
-        rolls.entry(Key((x + 1, y))).and_modify(|v| *v -= 1);
-        rolls.entry(Key((x - 1, y + 1))).and_modify(|v| *v -= 1);
-        rolls.entry(Key((x, y + 1))).and_modify(|v| *v -= 1);
-        rolls.entry(Key((x + 1, y + 1))).and_modify(|v| *v -= 1);
+        rolls.entry((x - 1, y - 1)).and_modify(|v| *v -= 1);
+        rolls.entry((x, y - 1)).and_modify(|v| *v -= 1);
+        rolls.entry((x + 1, y - 1)).and_modify(|v| *v -= 1);
+        rolls.entry((x - 1, y)).and_modify(|v| *v -= 1);
+        rolls.entry((x + 1, y)).and_modify(|v| *v -= 1);
+        rolls.entry((x - 1, y + 1)).and_modify(|v| *v -= 1);
+        rolls.entry((x, y + 1)).and_modify(|v| *v -= 1);
+        rolls.entry((x + 1, y + 1)).and_modify(|v| *v -= 1);
     }
 }
 
